4fe39aa3c8d6247798c8a8a4801f3f87103980da
jarvena <antti.p.jarvenpaa@aalto.fi>
2021-06-21 15:06:34 +0300

    live visualization 1.0

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import dash
+from dash.dependencies import Output
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import geopandas as gpd
+import pymongo
+import os
 import logging
+import datetime
+
+DB_URL = os.getenv('DB_URL')
+LIVE_LENGTH = datetime.timedelta(days=1)
+AJETTAVA = 'Lohja'
 
 logging.basicConfig(
     level=logging.INFO,
@@ -26,6 +36,26 @@ server = app.server
 logger.info('reading data')
 kunnat = gpd.read_file('./data/kunnatwgs84simplified.geojson')
 statsit = pd.read_csv('./data/ympäriajostatsit.csv', delimiter=';')
+
+# Setting up db connection
+def parse_locationdata(collection): # A function for reading location information from Mongo
+  data = collection.find({})
+  reitit = {'times': [], 'names': [], 'lats': [], 'lons': []}
+  kuvat = {}
+
+  for object in data:
+    if datetime.datetime.now() - object['route'][-1][-1] < LIVE_LENGTH:
+      reitit['lons'] = reitit['lons'] + list(map(lambda x: x[0], object['route']))
+      reitit['lats'] = reitit['lats'] + list(map(lambda x: x[1], object['route']))
+      reitit['times'] = reitit['times'] + list(map(lambda x: x[2], object['route']))
+      reitit['names'] = reitit['names'] + [object['details']['name']['first']]*len(object['route'])
+      kuvat[object['details']['name']['first']] = object['details']['photo']
+  
+  reittiDf = pd.DataFrame(reitit)
+  return reittiDf, kuvat
+
+client = pymongo.MongoClient(DB_URL)
+collection = client.routedata.routes
 
 # Formatting data
 logger.info('formatting data')
@@ -93,9 +123,18 @@ fig_osallistujaselviytymiset.update_layout(
   )
 fig_osallistujaselviytymiset.update_traces(hovertemplate='<b>%{x}</b><br>Selviytymiset=%{y}<extra></extra>')
 
+liveTracking = dbc.Row([
+  dbc.Col(
+    id='live-tracking-column',
+    width=12
+  ),
+  dcc.Interval(id='interval-component', interval=30*1000, n_intervals=0)
+])
+
 logger.info('defining layout')
 app.layout = dbc.Container([
     dbc.Row(dbc.Col(dbc.NavbarSimple(brand='Ympäriajotilastot', color='primary', dark=True), width=12), className='shadow mb-3'),
+    liveTracking,
     dbc.Row(
       dbc.Col(
         dbc.Card([
@@ -159,6 +198,82 @@ app.layout = dbc.Container([
   ],
   fluid=True
 )
+
+@app.callback(
+  Output('live-tracking-column', 'children'),
+  Input('interval-component', 'n_intervals'))
+def updateLive(n):
+  liveData, photos = parse_locationdata(collection)
+  if liveData.size == 0:
+    return None
+  
+  logger.info('Creating/updating live location tracking')
+
+  liveKartta = px.line_mapbox(liveData,
+    lat='lats',
+    lon='lons',
+    line_group='names',
+    mapbox_style="carto-positron",
+    hover_data={'names': True, 'times': "|%X"}
+  )
+  liveKartta.update_layout(
+    margin={"r":0,"t":0,"l":0,"b":0},
+    legend=dict(yanchor='top', y=0.99, xanchor='left', x=0.01),
+    legend_title_text='Selite',
+    mapbox={
+      'zoom': 8,
+      'center': {"lat":60.4, "lon":23.96}
+    },
+    uirevision='static' # to prevent ui reset at each update
+  )
+  liveKartta.update_traces(hovertemplate='<b>%{customdata[0]}</b><br>Nähty: %{customdata[1]|%H:%M}<extra></extra>')
+
+  # Highlighting the latest location update
+  latest_update = liveData.iloc[liveData['times'].idxmax()]
+  liveKartta.add_trace(
+    go.Scattermapbox(
+      lat=[latest_update.lats],
+      lon=[latest_update.lons],
+      name='Reitti ja sijainti',
+      mode='lines+markers',
+      marker=dict(
+        size=10,
+        color='#636dfa'
+      ),
+      hoverinfo='skip'
+    )
+  )
+
+  # Adding municipality border to the map
+  geometry=kunnat[kunnat['NAMEFIN']==AJETTAVA].geometry.iloc[0]
+  lats=geometry.boundary.coords.xy[1]
+  lons=geometry.boundary.coords.xy[0]
+  liveKartta.add_trace(
+    go.Scattermapbox(
+      lat=list(lats),
+      lon=list(lons),
+      mode='lines',
+      name='Kunnanraja',
+      hovertemplate=f'<b>{AJETTAVA}</b><extra></extra>',
+      line_color='#00cc96'
+    )
+  )
+
+  if datetime.datetime.now() - latest_update.times < datetime.timedelta(minutes=5):
+    badgeText = 'Live'; badgeColor='success'
+  else:
+    badgeText = 'Offline'; badgeColor='danger'
+
+  children = dbc.Card([
+    dbc.CardHeader([dbc.Badge(badgeText, color=badgeColor, className="mr-1"), 'Reaaliaikainen tilanneseuranta']),
+    dbc.CardBody([dcc.Graph(
+      id='live-kartta',
+      figure=liveKartta
+      )
+    ])
+  ], className='mb-3')
+
+  return children
 
 
 if __name__ == '__main__':
